84566e9f18d8ab17f3e9998c4a14af721eaf2e47
Sam Annetts <samannetts8@gmail.com>
2024-12-28 18:25:43 +0000

    Included a new gitignore file, converted my .db data file into a readable SQL data file and began building the Flask Backend
    During this, I also succesfully linked my database to pgadmin4

---
+++ flask_backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+load_dotenv('../env_docs/info.env') 
+database_username = os.getenv('database_username')
+database_password = os.getenv('database_password')
+database_name = os.getenv('database_name')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{database_username}:{database_password}!@localhost/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
