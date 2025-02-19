756f2fabfb2e1e202f1a8039612fdb7780638ae4
samannetts8 <samannetts8@gmail.com>
2025-02-19 20:53:31 +0000

    Logo added to homepage

--- vivino_react/src/App.tsx
+++ vivino_react/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DatabasePage from './pages/DatabasePage';
 
+
 function App() {
   return (
     <Router>
