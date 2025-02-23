0e6cb5461ab7ad7445434757e19b57777aa0457c
samannetts8 <samannetts8@gmail.com>
2025-02-23 00:03:30 +0000

    Initialised connection between Flask and Supabase

--- vivino_flask/server.py
+++ vivino_flask/server.py
@@ -1,13 +1,24 @@
-from flask import Flask;
+from flask import Flask,jsonify;
+from dotenv import load_dotenv
+from supabase import create_client, Client
+import os
+
+load_dotenv()
+
+supabase_url = os.getenv('supabase_url')
+supabase_key = os.getenv('supabase_key')
+supabase = create_client(supabase_url,supabase_key)
+print(f"Supabase client: {supabase}")
 
 app = Flask(__name__)
 
 
-@app.route("/database")
+@app.route("/")
 def members():
-    return {"Big Man Tings": ["Sam","Alistair","Carmen"]}
+    # Query data from Supabase
+    data = supabase.table('Vivino_data').select('*').execute()
+    return jsonify(data.data)  # Return the fetched data as JSON
 
 if __name__ == "__main__":
         app.run(debug=True)
 
-#test
